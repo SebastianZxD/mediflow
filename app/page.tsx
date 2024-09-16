7a722168f7b1740d3d608e6b85070aa62ac2a4b4
Sebastián Zambrano <sebastianzambrano@Sebastians-MacBook-Air.local>
2024-09-16 11:52:04 -0500

    Added Logo and Updated README

--- app/page.tsx
+++ app/page.tsx
@@ -14,26 +14,29 @@ export default function Home({ searchParams } : SearchParamProps) {
       {/* TODO: OTP Verification Pass Key Modal */}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
+          <div className="flex flex-row">
           <Image 
-            src="/assets/icons/logo-full.svg"
+            src="/assets/icons/logo-icon.webp"
             height={1000}
             width={1000}
             alt="Patient"
-            className="mb-12 h-[2.5rem] w-fit"
-          />
+            className="mb-12 h-[2.5rem] w-fit rounded-[5px] border border-blue-500"
+            />
+            <h1 className="font-bold text-3xl ml-2">Medi Flow</h1>
+          </div>
           <PatientForm />
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
               © 2024 MediFlow
             </p>
-            <Link href="/?admin=true" className="text-green-500">
+            <Link href="/?admin=true" className="text-blue-500">
               Admin
             </Link>
           </div>
         </div>
       </section>
       <Image 
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/onboarding-img.webp"
         height={1000}
         width={1000}
         alt="patient"
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -56,7 +56,7 @@ const PatientForm = () => {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
         <section className="mb-12 space-y-4">
-          <h1 className="header">Hi there 👋🏼</h1>
+          <h1 className="header">Welcome! 👋🏼</h1>
           <p className="text-dark-700">Schedule your first appointment.</p>
         </section>
         <CustomFormField
